e40eac2e2fb02a5e90fe50a14d99b4c983c1c6f3
starrynight <starrynight1926@gmail.com>
2025-07-01 09:19:11 +0700

    [FE] Html template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaticPageController;
+use App\Http\Controllers\ForwardController;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Static pages
+Route::get('/login', [StaticPageController::class, 'login'])->name('static.login');
+Route::get('/register', [StaticPageController::class, 'register'])->name('static.register');
+Route::get('/', [StaticPageController::class, 'index'])->name('static.index');
+
+//Test routes
+Route::post('/test', [ForwardController::class, 'index'])->name('test.index');
