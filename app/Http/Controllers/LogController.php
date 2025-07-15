93747513bb5fbe66df28ec700dc47eb0cc770a60
Snow Deep <barondeepsnow@gmail.com>
2025-07-15 22:05:51 +0700

    [Fix] quickfix 15-07

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -10,17 +10,17 @@ class LogController extends Controller
     public function webhook(Request $request)
     {
         $data = $request->all();
+        // $title = $data['body']['event'] ?? 'No Title';
+        // return response()->json(['status' => 'ok','data' => $data,'title' => $title]);
+
+
         $title = $data['body']['event'] ?? 'No Title';
-        return response()->json(['status' => 'ok','data' => $data,'title' => $title]);
+         Log::create([
+            'title' => $title,
+            'data' => $data
+        ]);
 
-
-        // $title = $data[0]['body']['event'] ?? 'No Title';
-        //  Log::create([
-        //     'title' => $title,
-        //     'data' => $data
-        // ]);
-
-        // return response()->json(['status' => 'ok','data' => $data]);
+        return response()->json(['status' => 'ok','data' => $data]);
     }
     public function index(Request $request)
     {
