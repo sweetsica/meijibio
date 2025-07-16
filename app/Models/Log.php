12fe294728e0893958ee34eeb72f84e5c4d8070b
starrynight <starrynight1926@gmail.com>
2025-07-16 15:25:37 +0700

    [fix] webhook function

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Log extends Model
 {
-    protected $fillable = ['title', 'data'];
+    protected $fillable = ['title', 'noti_data', 'data'];
 
     protected $casts = [
+        'noti_data' => 'array',
         'data' => 'array',
     ];
 }
