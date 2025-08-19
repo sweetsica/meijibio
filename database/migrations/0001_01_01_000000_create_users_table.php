c88eb5eb9038289f9748c3ba64e0cc029dd835f9
sweetsica <barondeepsnow@gmail.com>
2025-08-19 23:42:38 +0700

    fix seed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('getfly_id')->nullable();
             $table->string('area')->default(0);
             $table->string('name');
             $table->string('code')->nullable();
